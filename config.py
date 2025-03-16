e46afb57eb5d131578f0e2760fff465d681012b3
Er. Amal Raj <amalsri225@gmail.com>
2025-03-17 00:53:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "your-rds-endpoint.rds.amazonaws.com"
-customuser = "your-db-user"
-custompass = "your-db-password"
-customdb = "your-db-name"
+customhost = "your-db-instance.abc12345.us-east-1.rds.amazonaws.com"  # Replace this
+customuser = "admin"
+custompass = "yourpassword"
+customdb = "employees"
 custombucket = "your-s3-bucket"
 customregion = "us-east-1"
